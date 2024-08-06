ed0375d1f1bb94eb10d2a3a559180e6b8fccd7b7
ayu3456 <coaderayush1309@gmail.com>
2024-08-06 15:42:02 +0530

    completed the assingment

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
+const fs = require('fs')
 function logInfo(message) {
-
+  const timeStamps = new Date().toISOString();
+  const logmessage = `${timeStamps} | INFO | ${message} \n`
+  fs.appendFileSync("./logs/app.log",logmessage)
 }
-
 function logWarning(message) {
-
+  const timeStamps = new Date().toISOString();
+  const warnMessage = `${timeStamps} | WARNING | ${message} \n`
+  fs.appendFileSync("./logs/app.log",warnMessage)
 }
-
 function logError(message) {
-
+  const timeStamps = new Date().toISOString();
+  const errorMessage = `${timeStamps} | ERROR | ${message} \n`
+  fs.appendFileSync("./logs/app.log",errorMessage)
 }
-
 logInfo("This is an info message");
 logWarning("This is a warning message");
 logError("This is an error message");
-
 module.exports = {
   logInfo,
   logWarning,
   logError,
 };
+
+
+
+
